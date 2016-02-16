0597fa906d8dbec9497500efdb7b5c0c7ca99997
Sindt <czindt@gmail.com>
2016-02-16 14:24:10 +0100

    .js added

--- reusable-modules.js
+++ reusable-modules.js
@@ -1,3 +1,22 @@
 /**
  * Created by Sindt on 15-02-2016.
  */
+var personFunc = function(){
+    var person = {name: "Bob", age:12 };
+    function setAge(val){
+        person.age = val;
+    }
+    function setName(val){
+        person.name = val;
+    }
+    function getInfo(){
+        return person.name + " " + person.age;
+    }
+    return {
+        setAgeFunc: function(val){setAge(val);},
+        setNameFunc: function(val){setName(val);},
+        getInfoFunc: function(){return getInfo();}
+    }
+};
+
+console.log(personFunc().getInfoFunc());
--- use-strict.js
+++ use-strict.js
@@ -1,3 +1,10 @@
 /**
  * Created by Sindt on 15-02-2016.
  */
+"use strict";
+myFunction();
+
+function myFunction() {
+    y = 3.14;   // This will cause an error (y is not defined)
+}
+
